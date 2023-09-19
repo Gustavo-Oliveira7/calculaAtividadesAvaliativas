b56fd1c4927f3f2429705362948cb5066a1df8a1
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-09-19 15:10:15 -0300

    req 1 e 2

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,36 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
 
   /**
-   * Metodo main
+   * Metodo main!.
    */
   public static void main(String[] args) {
+    cadastrarAtividadesAvaliativas();
+  }
+
+  /**
+   * Método cadastra atividades.
+   */
+
+  public static void cadastrarAtividadesAvaliativas() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int count = Integer.parseInt(scanner.nextLine());
+    for (int i = 1; i <= count; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String nomeAtividade = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + i + ":");
+      int pesoAtividade = Integer.parseInt(scanner.nextLine());
+      System.out.println("Digite a nota obtida para " + nomeAtividade + ":");
+      int notaAtivdade = Integer.parseInt(scanner.nextLine());
+
+    }
+    scanner.close();
   }
 }
