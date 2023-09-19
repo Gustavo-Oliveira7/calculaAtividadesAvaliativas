a76041de9f2f9a20f2ed3b61f8fec751b93404b3
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-09-19 15:36:03 -0300

    req3

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -22,15 +22,41 @@ public class App {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     int count = Integer.parseInt(scanner.nextLine());
-    for (int i = 1; i <= count; i++) {
-      System.out.println("Digite o nome da atividade " + i + ":");
+
+    int[] arrayPeso;
+    int[] arrayNota;
+    int total = 0;
+    int peso = 0;
+    arrayPeso = new int[count];
+    arrayNota = new int[count];
+
+    for (int i = 0; i < count; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
       String nomeAtividade = scanner.nextLine();
-      System.out.println("Digite o peso da atividade " + i + ":");
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
       int pesoAtividade = Integer.parseInt(scanner.nextLine());
+      arrayPeso[i] = pesoAtividade;
       System.out.println("Digite a nota obtida para " + nomeAtividade + ":");
       int notaAtivdade = Integer.parseInt(scanner.nextLine());
+      arrayNota[i] = notaAtivdade;
 
     }
     scanner.close();
+
+    for (int i = 0; i < count; i++) {
+      int nota = arrayNota[i] * arrayPeso[i];
+      peso += arrayPeso[i];
+      total += nota;
+    }
+    int notaTotal = total / peso;
+    if (notaTotal < 85) {
+      System.out.println("Lamentamos informar que, "
+          + "com base na sua pontuação alcançada neste período, " + notaTotal + ".0%, "
+          + "você não atingiu a pontuação mínima necessária para sua aprovação.");
+    } else {
+      System.out.println("Parabéns! Você alcançou " + notaTotal + ".0%! "
+          + "E temos o prazer de informar que você "
+          + "obteve aprovação! ");
+    }
   }
 }
